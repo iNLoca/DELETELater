5fb4e22e5dbf7f0b35b567d91bea715e1b2dee3e
iNLoca <56917419+iNLoca@users.noreply.github.com>
2020-01-07 17:05:07 +0100

    Changed name to controller

--- FindYourFavs/src/anything/Controller.java
+++ FindYourFavs/src/anything/Controller.java
@@ -15,7 +15,7 @@ import javafx.stage.Stage;
  *
  * @author mac
  */
-public class Anything extends Application {
+public class Controller extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
