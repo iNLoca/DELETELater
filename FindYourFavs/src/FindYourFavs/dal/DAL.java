d3dbdc90342072f3ca50c7d93d7fdc2fad3da4f0
Francesco-hub <56824952+Francesco-hub@users.noreply.github.com>
2020-01-08 16:16:29 +0100

    yyy

--- FindYourFavs/src/FindYourFavs/dal/DAL.java
+++ FindYourFavs/src/FindYourFavs/dal/DAL.java
@@ -19,7 +19,7 @@ import java.util.logging.Logger;
 public class DAL {
 
     private SQLServerDataSource ds;
-    private Manager man= new Manager();
+    //private Manager man= new Manager();
 
     public DAL() {
         ds = new SQLServerDataSource();
