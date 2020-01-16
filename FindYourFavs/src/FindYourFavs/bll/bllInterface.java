63c2fab10e663cf6b7afd2d635dc1451ef14a81e
iNLoca <56917419+iNLoca@users.noreply.github.com>
2020-01-16 15:25:48 +0100

    Interface GoodLuck ^^

---
+++ FindYourFavs/src/FindYourFavs/bll/bllInterface.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FindYourFavs.bll;
+
+/**
+ *
+ * @author mac
+ */
+public interface bllInterface {
+    
+    public int getID(int idItself);
+    
+    public void createMovie(String name, int time);
+    
+}
---
+++ FindYourFavs/src/FindYourFavs/bll/bllimp.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FindYourFavs.bll;
+
+/**
+ *
+ * @author mac
+ */
+public class bllimp implements bllInterface {
+
+    @Override
+    public int getID(int idItself) {
+         idItself=0;
+          return idItself;
+    }
+
+    @Override
+    public void createMovie(String name, int time) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
